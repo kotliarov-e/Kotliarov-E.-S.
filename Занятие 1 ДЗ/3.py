a0843937cd20ec924968c77cea3e8a3e2bb211d9
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2021-09-25 19:19:36 +0300

    Delete Занятие 1 ДЗ directory

--- Занятие 1 ДЗ/3.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-
-# №4
-seconds = int(input("Введите количество секунд: "))
-sec = (str(seconds // 86400), str(round(seconds / 3600, 2)), str(round(seconds / 60, 2)), str(seconds))
-print(f"{seconds} секунд это:", sec)
--- Занятие 1 ДЗ/6.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-
-# №6
-x, y = input("x: "), input("y: ")
-a = x, y = y, x
-print(' '.join(a))

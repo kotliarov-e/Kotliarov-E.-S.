b7975d4c2712ea28d29cc033b53e7ffd0b499b22
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2021-09-25 18:04:11 +0300

    Add files via upload

---
+++ Занятие 1 ДЗ/n4.py
@@ -0,0 +1,5 @@
+# -- coding: utf-8 --
+
+# №5
+n = int(input("Введите число n: "))
+print(n + n**2 + n**3 + n**4 + n**5)
---
+++ Занятие 1 ДЗ/n5.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+
+# №6
+x, y = input("x: "), input("y: ")
+a = x, y = y, x
+print(' '.join(a))

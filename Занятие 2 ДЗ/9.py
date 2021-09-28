db3e69b13377b5e5824b3420a467b0e9a0491d09
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2021-09-28 21:54:11 +0300

    Add files via upload

---
+++ Занятие 2 ДЗ/9.py
@@ -0,0 +1,6 @@
+def shok(n, m, k):
+    if (k < n * m) and (k % n == 0 or k % m == 0):
+        return 'Да'
+    else:
+        return 'Нет'
+print(shok(abs(int(input('n: '))), abs(int(input('m: '))), abs(int(input('k: ')))))

6471c1fc5e2532db63db4000ea2471b942bd7f3b
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2021-12-11 20:14:02 +0300

    Update 5.py

--- Занятие 5 ДЗ/5.py
+++ Занятие 5 ДЗ/5.py
@@ -1,15 +1,21 @@
 # -- coding: utf-8 --
 def f5():
     k = 0
-    n = int(input('n: '))
-    if n >= 0:
-        while n != 0:
-            n = int(input('n: '))
-            if n >= 0:
-                k += 1
-            else:
-                return print('n < 0')
-        print(k)
-    else:
-        print('n < 0')
+    while int(input('n: ')) > 0:
+        k += 1
+    print(k)
 f5()
+# def f5():
+#     k = 0
+#     n = int(input('n: '))
+#     if n >= 0:
+#         while n != 0:
+#             n = int(input('n: '))
+#             if n >= 0:
+#                 k += 1
+#             else:
+#                 return print('n < 0')
+#         print(k)
+#     else:
+#         print('n < 0')
+# f5()

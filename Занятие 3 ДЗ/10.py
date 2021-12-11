315a27f33a606fb1db60753f456809ab326e83b1
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2021-12-11 17:34:20 +0300

    Update 10.py

--- Занятие 3 ДЗ/10.py
+++ Занятие 3 ДЗ/10.py
@@ -1,9 +1,15 @@
-# -- coding: utf-8 --
-def n10(n, k):
-    F1, F2 = 1, 1
-    m = [F1, F2]
-    for i in range(2, n):
-        F1, F2 = F2, F1 + F2
-        m.append(F2)
-    print(sum(m[k-1:]))     #я не придумал другого способа
-n10(int(input('Кол-во чисел из ряда: ')), int(input('Номер с которого начать: ')))
+def f10():
+    def fib(n): # 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
+        if n < 2:
+            return n
+        else:
+            return (fib(n - 1) + fib(n - 2))
+    k = int(input('Кол-во чисел: '))
+    n = int(input('С какого эл-та начать: '))
+    s = 0
+    for i in range(k):
+        n1 = fib(n)
+        s += n1
+        n += 1
+    print('Сумма:', s)
+f10()

d2247448183e41962a3674c32353f3c19e0b801d
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2021-12-13 10:56:09 +0300

    Update 6.py

--- Занятие 5 ДЗ/6.py
+++ Занятие 5 ДЗ/6.py
@@ -3,9 +3,10 @@ def f6():
     k = 0
     s = 0
     n = int(input('n: '))
-    while n > 0:
+    while n != 0:
         s += n
         k += 1
         n = int(input('n: '))
-    print(s / k)
+    else:
+        print(s / k)
 f6()

86875554592f8b18ee835a418a5802996d16e6db
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2022-05-22 17:49:09 +0300

    Update z4.java

--- Практическая №2/z4.java
+++ Практическая №2/z4.java
@@ -1,20 +1,22 @@
-package prakt2;
+// Дан двумерный массив А, размером (n×n) (или квадратная матрица А). 
+// Найти среднее арифметическое положительных элементов параллели главной диагонали, 
+// расположенной выше над диагональю.
 
 public class z4 {
 	public static void main(String n[]) {
 		int n1 = 5;
 		int[][] array = new int[n1][n1];
+		double sum = 0, sar = 0;
+		int count = 0;
 		
 		for (int i = 0; i < array.length; i++) {
 			for (int j = 0; j < array.length; j++) {
-				array[i][j] = (int)(Math.random()*5);
-				System.out.print(array[i][j] + " | ");
+				array[i][j] = (int)(Math.random()*20 - 10);
+				System.out.print(array[i][j] + "  ");
 			}
 			System.out.println();
 		}
 		
-		double sum = 0, sar = 0;
-		int count = 0;
         for (int i = 0; i < array.length; i++) {        	
         	for (int j = 0; j < array.length; j++) {
         		if ((array[i][j] > 0) && (i < j) && (j - i == 1)) {  
@@ -23,6 +25,7 @@ public class z4 {
                 }
             }
         }
+		
         if (count > 0) {
     		sar = sum / count; 
     	}

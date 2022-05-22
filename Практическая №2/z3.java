9ed6bf4917f45c7e6d8f5e92c2d29ebae5a0d1f4
kotliarov-e <91281602+kotliarov-e@users.noreply.github.com>
2022-05-22 17:25:30 +0300

    Add files via upload

---
+++ Практическая №2/z3.java
@@ -0,0 +1,33 @@
+package prakt2;
+
+public class z3 {
+  public static void main(String n[]) {
+    double[][] array = new double[5][5];
+
+    for (int i = 0; i < array.length; i++) {
+      for (int j = 0; j < array.length; j++) {
+        array[i][j] = (double)(Math.round(Math.random()*20 - 10));
+        System.out.print(Math.round(array[i][j]) + " | ");
+      }
+      System.out.println();
+    }
+
+    double[] tempVector = new double[array.length];
+    double count;
+        for (int i = 0; i < array.length; i++) {  
+        	count = 0;
+        	tempVector[i] = tempVector[i] + 1;
+          for (int j = 0; j < array.length; j++) {
+                if (array[j][i] > 0) {  
+                	count++;         	
+                    tempVector[i] *= array[j][i]; 
+                }
+          }
+          tempVector[i] = Math.pow(tempVector[i], 1 / count);
+        }
+
+        System.out.println("Вектор В:");      
+        for (double i : tempVector)
+            System.out.print(Math.round(i) + " | ");
+  }
+}
---
+++ Практическая №2/z4.java
@@ -0,0 +1,31 @@
+package prakt2;
+
+public class z4 {
+	public static void main(String n[]) {
+		int n1 = 5;
+		int[][] array = new int[n1][n1];
+		
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array.length; j++) {
+				array[i][j] = (int)(Math.random()*5);
+				System.out.print(array[i][j] + " | ");
+			}
+			System.out.println();
+		}
+		
+		double sum = 0, sar = 0;
+		int count = 0;
+        for (int i = 0; i < array.length; i++) {        	
+        	for (int j = 0; j < array.length; j++) {
+        		if ((array[i][j] > 0) && (i < j) && (j - i == 1)) {  
+                	count++;
+                    sum += array[i][j]; 
+                }
+            }
+        }
+        if (count > 0) {
+    		sar = sum / count; 
+    	}
+        System.out.println("Среднее арифметическое параллели главной диагонали: " + sar);      
+	}
+}
